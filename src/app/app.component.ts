dd9ef830102d63ec477839db0b4245a106dfeea4
Roman Maruk <maruchok@gmail.com>
2023-05-24 14:26:19 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Got_IT_Practic_TEST_Angular_15';
+}
