19db0bfe90455194d508b58d50881f46aa454b90
Roman Maruk <maruchok@gmail.com>
2023-05-24 16:08:19 +0300

    create routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShowTypeComponent } from './components/show-type/show-type.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ShowTypeComponent },
+  { path: '', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
