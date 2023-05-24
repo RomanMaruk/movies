fd7c0ef95db693186bad6518f3f19a1ada7b47b5
Roman Maruk <maruchok@gmail.com>
2023-05-24 15:45:28 +0300

    create type show component

---
+++ src/app/components/show-type/show-type.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShowTypeComponent } from './show-type.component';
+
+describe('ShowTypeComponent', () => {
+  let component: ShowTypeComponent;
+  let fixture: ComponentFixture<ShowTypeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ShowTypeComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ShowTypeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/show-type/show-type.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-type',
+  templateUrl: './show-type.component.html',
+  styleUrls: ['./show-type.component.scss']
+})
+export class ShowTypeComponent {
+
+}
