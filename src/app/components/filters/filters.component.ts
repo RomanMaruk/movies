c54d9c208c74e7ab0ea08b9a0943729b5a86d2db
Roman Maruk <maruchok@gmail.com>
2023-05-24 17:01:32 +0300

    create search name

--- src/app/components/filters/filters.component.ts
+++ src/app/components/filters/filters.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html',
-  styleUrls: ['./filters.component.scss']
+  styleUrls: ['./filters.component.scss'],
 })
-export class FiltersComponent {
+export class FiltersComponent implements OnInit {
+  @Output() emitName = new EventEmitter<string>();
 
+  name = new FormControl('');
+
+  ngOnInit(): void {
+    this.name.valueChanges.subscribe((text) => {
+      this.emitName.emit(text ?? '');
+    });
+  }
 }
